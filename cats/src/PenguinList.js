b3c737c373d5b5a527a828ac412cd9d10aaef397
AmyFong82 <amyhfong@gmail.com>
2021-10-21 11:35:31 -0400

    added Penguin & PenguinList.js

--- cats/src/PenguinList.js
+++ cats/src/PenguinList.js
@@ -1,5 +1,13 @@
 import React, {Component} from "react"
+import {penguins} from './penguin_data'
+import Penguin from './Penguin'
 
 export default class PenguinList extends Component {
-  
+  penguinList = penguins.map(penguin => <Penguin key={penguin.id} penguin={penguin} />)
+
+  render(){
+    return(
+      <div>{this.penguinList}</div>
+    )
+  }
 }
