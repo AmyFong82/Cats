31945a6bd017d1a81dcdf359dd8be72a1be254cb
AmyFong82 <amyhfong@gmail.com>
2021-10-25 09:41:29 -0400

    added dragon_data.js

---
+++ cats/src/dragon_data.js
@@ -0,0 +1,18 @@
+export const dragons = [
+  {id: 1,
+  name: "AD",
+  gender: "boy"}
+    {id: 2,
+  name: "BD",
+  gender: "boy"}
+    {id: 3,
+  name: "CD",
+  gender: "girl"}
+    {id: 4,
+  name: "DD",
+  gender: "girl"}
+    {id: 5,
+  name: "ED",
+  gender: "boy"}
+  
+]
