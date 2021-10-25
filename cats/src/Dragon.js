87341744fbb1641ec071f3f30deaa27754d7579c
AmyFong82 <amyhfong@gmail.com>
2021-10-25 09:52:26 -0400

    Added Dragon Component

---
+++ cats/src/Dragon.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import DragonList from './DragonList';
+
+export default class Dragon extends Component {
+  state={
+    like: 0
+  }
+
+  handleClick = () => {
+    this.setState(prevState => 
+      like: prevState.like + 1)
+  }
+  render(){
+    return(
+      <li key={this.key}>Name: {this.dragon.name}<button onClick={this.handleClick}>{this.state.like}</button></li>
+    )
+  }
+}
