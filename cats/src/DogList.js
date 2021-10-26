a3c7d5bb8ba0fceb5ebe4d9e8f41bb8b62f72082
AmyFong82 <amyhfong@gmail.com>
2021-10-26 18:05:52 -0400

    adding filter btn for dog breeds.

--- cats/src/DogList.js
+++ cats/src/DogList.js
@@ -5,11 +5,13 @@ import Dog from './Dog'
 export default class DogList extends Component {
 
   doglist = dogs.map(dog => <Dog key={dog.id} dog={dog} />)
+  filters = dogs.map(dog => <Button key={dog.breed} breed ={dog.breed} />)
 
   render(){
     return(
       <div>
         <h1>Dog List</h1>
+        {this.filters}
         {this.doglist}
       </div>
       
