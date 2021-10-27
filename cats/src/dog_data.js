4350ebb46fcbf57cb6d9b91823a930d903273af4
AmyFong82 <amyhfong@gmail.com>
2021-10-27 10:30:35 -0400

    added age to the dog_data

--- cats/src/dog_data.js
+++ cats/src/dog_data.js
@@ -3,36 +3,42 @@ export const dogs = [
     id: 1,
     name: "Toto",
     breed: "Cairn Terrier",
-    knownFor: "Wizard of Oz"
+    knownFor: "Wizard of Oz",
+    age: 1
   },
   {
     id: 2,
     name: "Rin Tin Tin",
     breed: "German Shepherd",
-    knownFor: "Movie Star"
+    knownFor: "Movie Star",
+    age: 11
   },
   {
     id: 3,
     name: "Lassie",
     breed: "Rough Collie",
-    knownFor: "Lassie Tv Show"
+    knownFor: "Lassie Tv Show",
+    age: 2
   },
   {
     id: 4,
     name: "Trakr",
     breed: "German Shepherd",
-    knownFor: "Responder during 9/11"
+    knownFor: "Responder during 9/11",
+    age: 4
   },
   {
     id: 5,
     name: "Bobbie the Wonder Dog",
     breed: "German Shepherd",
-    knownFor: "Gotta be the inspiration for Homeward Bound"
+    knownFor: "Gotta be the inspiration for Homeward Bound",
+    age: 6
   },
   {
     id: 6,
     name: "Balto",
     breed: "Siberian Husky",
-    knownFor: "Important Delivery"
+    knownFor: "Important Delivery",
+    age: 3
   }
 ];
