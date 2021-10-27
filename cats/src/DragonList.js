a1ffa3fed2578e4a4bc6e8adf208613a03df00ef
AmyFong82 <amyhfong@gmail.com>
2021-10-27 10:27:56 -0400

    back to original simple setState, becuase if no global state is set, the like count will not persist when switching filters to show different dragons.

--- cats/src/DragonList.js
+++ cats/src/DragonList.js
@@ -4,43 +4,22 @@ import Dragon from './Dragon'
 
 export default class DragonList extends Component {
   state = {
-    boyDragons: dragons.filter(dragon => dragon.gender === "boy"),
-    girlDragons: dragons.filter(dragon => dragon.gender === "girl"),
-    filter: "all"
+    dragons: dragons
   }
 
-  allDragonList = () => {
-    const allDragons = [...this.state.boyDragons, ...this.state.girlDragons]
-    return allDragons.map(dragon => <Dragon key={dragon.id} dragon={dragon} />)
-  }
+  dragonList = () => this.state.dragons.map(dragon => <Dragon key={dragon.id} dragon={dragon} />)
 
 
   showBoys = () => {
-    this.setState({
-      dragons: dragons.filter(dragon => dragon.gender === "boy")
-    })
-  };
-
+    this.setState({dragons: dragons.filter(dragon => dragon.gender === "boy")})
+  }
 
   showGirls = () => {
-    this.setState({
-      dragons: dragons.filter(dragon => dragon.gender === "girl")
-    })
+    this.setState({dragons: dragons.filter(dragon => dragon.gender === "girl")})
   }
 
-  showAll = () => {
-    this.setState({dragons: dragons})
-  }
+  showAll = () => this.setState({dragons: dragons})
 
-  dragonList = () => {
-    if(this.state.filter === "all"){
-      return this.allDragonList()
-    }else if(this.state.filter === "boys"){
-      this.showBoys()
-    }else{
-      this.showGirls()
-    }
-  }
 
   render(){
     return(
