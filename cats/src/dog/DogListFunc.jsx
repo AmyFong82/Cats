15946b6924deb6472815ca6f2dafadc6d452de4d
AmyFong82 <amyhfong@gmail.com>
2021-11-22 10:31:06 -0500

    added random dog image from fetch using useEffect.

--- cats/src/dog/DogListFunc.jsx
+++ cats/src/dog/DogListFunc.jsx
@@ -2,12 +2,20 @@ import {useEffect, useState} from 'react'
 import { dogs } from './dogs_data'
 
 export function DoglistFunc() {
+  const [dogpic, setDogpic] = useState(["I'm a picture"])
 
   const doglist = dogs.map(dog => <li key={dog.id}>Name: {dog.name}, Age: {dog.age}, Sex: {dog.sex}</li>)
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(resp => resp.json())
+    .then(resp => setDogpic(resp.message))
+  }, [])
 
   return(
     <div>
       <h1>Doglist</h1>
+      <img src={dogpic} alt="random dog image" />
       {doglist}
     </div>
   )
