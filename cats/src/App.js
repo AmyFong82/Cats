7071eb64d113394206327457b25bb8331860541c
AmyFong82 <amyhfong@gmail.com>
2021-11-22 10:10:37 -0500

    corrected the closing } in li.

--- cats/src/App.js
+++ cats/src/App.js
@@ -3,11 +3,13 @@ import CatList from './cat/CatList';
 import DogList from './dog/DogList';
 import PenguinList from './penguin/PenguinList';
 import DragonList from './dragon/DragonList';
+import { DoglistFunc } from './dog/DogListFunc';
 
 
 function App() {
   return (
     <div>
+      <DoglistFunc />
       <CatList />
       <DogList />
       <PenguinList />
